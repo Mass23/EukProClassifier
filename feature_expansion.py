4357470fef3d087d1375cf127ad9f232645bcb3e
Natasa <natasa.krco@epfl.ch>
2020-11-26 20:17:14 +0000

    feature expansion (small changes)

--- feature_expansion.py
+++ feature_expansion.py
@@ -4,20 +4,21 @@ from sklearn.svm import SVC
 from sklearn.svm import LinearSVC
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
 from helpers import *
-
-
-def 
-
+import pandas as pd
 
 ################## script
 
 max_degree = 4
 n_jobs = None
 seed = 42
+filename = "small_train.csv"
+#filename = "Counts_n10000_k5_s5000.csv"
 
-lin_svc = svm.LinearSVC(random_state=seed)
-ker_svc = svm.SVC(random_state=seed)
+
+lin_svc = LinearSVC(random_state=seed)
+svc = SVC(random_state=seed)
 log_reg = LogisticRegression(max_iter=10000, random_state=seed)
 rf = RandomForestClassifier(max_depth=10, random_state=seed)
 nn = MLPClassifier(solver='adam', max_iter=500, random_state=seed)
@@ -25,6 +26,9 @@ methods = {'linear svc':lin_svc, 'rbf svc':svc, 'logistic regression':log_reg, '
 
 df = pd.DataFrame(columns=['method', 'degree', 'accuracy', 'euk_acc', 'pro_acc',
                            'learning time', 'prediction time'])
+
+y, X, _ = load_csv_data(filename)
+
 
 datas = {0 : X}
 for i in range(max_degree):
