ff93c9da1e0738897b5975fb2662f238d805e23c
Mass23 <33321080+Mass23@users.noreply.github.com>
2020-12-16 10:49:25 +0100

    Update tune_methods.py

--- src/tune_methods.py
+++ src/tune_methods.py
@@ -25,23 +25,28 @@ cv = 5
 verbose = 2
 
 #######################################
-cv = 2
 y, X, _ = load_csv_data(FILENAME)
-X, y = X[:100], y[:100]
-data_char = 'freq_k5'
+X_freq = FREQ_transform(X)
+X_clr = CLR_transform(X_freq)
+
+k_list = np.arange(start=X.shape[1]//16, stop=X.shape[1], step=X.shape[1]//16)
 #######################################
 
 # define X, y and data_char for the correct transformation for linSVC
-grid_search_linSVC(X, y, data_char, cv, seed, verbose)
+grid_search_linSVC(X_clr, y, 'LinearSVC_CLR', cv, seed, verbose)
 
-# define X, y and data_char for the correct transformation for SVC
-grid_search_SVC(X, y, data_char, cv, seed, verbose)
+grid_search_SVC(X_clr, y, 'KernelSVC_CLR', cv, seed, verbose)
 
-# define X, y and data_char for the correct transformation for LogReg
-grid_search_LogReg(X, y, data_char, cv, seed, verbose)
+for k in k_list:
+    print(k)
+    kmeans = KMeans(n_clusters=k, random_state=seed).fit(X_freq.T)
+    kX = create_kmeans_data(X_freq, kmeans.labels_)
+    grid_search_SVC(kX, y, 'KernelSVC_Kmeans_k={}'.format(k), cv, seed, verbose)
+    grid_search_LogReg(kX, y, 'LogReg_Kmeans_k={}'.format(k), cv, seed, verbose)
+    grid_search_linSVC(kX, y, 'LinearSVC_Kmeans_k={}'.format(k), cv, seed, verbose)
 
 # define X, y and data_char for the correct transformation for RF
-grid_search_RF(X, y, data_char, cv, seed, verbose)
+grid_search_RF(X_freq, y, 'RandomForest_Freq', cv, seed, verbose)
 
 # define X, y and data_char for the correct transformation for NN
-grid_search_NN(X, y, data_char, cv, seed, verbose)
+grid_search_NN(X_clr, y, 'NeuralNetworks_CLR', cv, seed, verbose)
